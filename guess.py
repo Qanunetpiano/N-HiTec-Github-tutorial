010e506ad4ebbd862c8367e4366aa98fc4ef8231
Boustani Mehdi <sckapa.bst@gmail.com>
2025-03-19 11:37:21 +0100

    feat: adding guess.py

---
+++ guess.py
@@ -0,0 +1,21 @@
+import random
+
+def guess_number():
+    
+    number = random.randint(1, 100)
+    attempts = 0
+
+    while True:
+        guess = input("Devinez le nombre entre 1 et 100 : ")
+
+        if guess == number:
+            print("Bravo ! Vous avez trouvé le nombre en", attempts, "essais.")
+            break
+        elif guess < number:
+            print("Trop petit !")
+        else:
+            print("Trop grand !")
+
+        attempts += 1
+
+guess_number()
